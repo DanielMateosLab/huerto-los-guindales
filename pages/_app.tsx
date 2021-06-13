5db94e43a0707df72ad0450f51baf09a52f617a3
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-06-13 20:54:51 +0200

    Move Head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Huerto Los Guindales</title>
+        <meta
+          name="description"
+          content="Aplicación para la gestión de los cultivos de Los Guindales"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
-import Head from "next/head"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Huerto Los Guindales</title>
-        <meta
-          name="description"
-          content="Aplicación para la gestión de los cultivos de Los Guindales"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <h1 className={styles.title}>Huerto Los Guindales</h1>
       </main>
