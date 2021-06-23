a478f038576a8954398163df240b7e1062dc76a8
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-06-23 20:37:22 +0200

    Fetch and display groups

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,8 +1,13 @@
-import { GetServerSideProps } from "next"
-import connectToDb from "server/connectToDb"
+import { GetServerSideProps, InferGetServerSidePropsType } from "next"
+import { connectToDb, getClient } from "server/db"
+import { GroupsDAO } from "server/GroupsDAO"
+import { Group } from "utils/types"
 import styles from "../styles/Basic.module.css"
 
-export default function Admin() {
+export default function Admin(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  console.log(props)
   return (
     <div className={styles.container}>
       <main>
@@ -11,14 +16,34 @@ export default function Admin() {
         </header>
         <section>
           <h2>Grupos</h2>
+          {props.groups &&
+            props.groups.map((group: Group) => (
+              <div key={group._id}>{group.name}</div>
+            ))}
         </section>
       </main>
     </div>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<{
+  groups?: any
+  error?: any
+}> = async () => {
+  const client = getClient()
   try {
-    const { db } = await connectToDb()
-  } catch (error) {}
+    const db = await connectToDb(client)
+    const groupsDAO = new GroupsDAO(db)
+
+    const groups = await groupsDAO.getGroups()
+    return {
+      props: { groups },
+    }
+  } catch (error) {
+    return {
+      props: { error },
+    }
+  } finally {
+    await client.close()
+  }
 }
