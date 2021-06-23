84c31a8bc032a0ae4b17d7d01faabd81d730d217
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-06-23 20:36:38 +0200

    Type GroupsDAO collection prop.

--- server/GroupsDAO.ts
+++ server/GroupsDAO.ts
@@ -1,9 +1,20 @@
 import { Collection, Db } from "mongodb"
+import { Group } from "utils/types"
 
 export class GroupsDAO {
-  private collection: Collection
+  private collection: Collection<Group>
 
   constructor(db: Db) {
-    this.collection = db.collection("groups")
+    this.collection = db.collection<Group>("groups")
+  }
+
+  async getGroups() {
+    const groups = await this.collection
+      .find()
+      .sort({ name: 1 })
+      .limit(10)
+      .toArray()
+
+    return groups.map((group) => ({ ...group, _id: group._id.toString() }))
   }
 }
